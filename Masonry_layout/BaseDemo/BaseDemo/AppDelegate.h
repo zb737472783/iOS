0e0cceac050ffec2da9e3b119d9375b1688f7d14
zhengbo <zb737472783@me.com>
2015-11-27 18:25:55 +0800

    初始化工程,引入 Masonry 框架
    等待写入代码

---
+++ Masonry_layout/BaseDemo/BaseDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseDemo
+//
+//  Created by 星尘 on 15/11/27.
+//  Copyright © 2015年 星尘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Masonry_layout/BaseDemo/BaseDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseDemo
+//
+//  Created by 星尘 on 15/11/27.
+//  Copyright © 2015年 星尘. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
